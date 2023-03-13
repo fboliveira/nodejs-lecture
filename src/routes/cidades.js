c92dc062360c47e8915d5670c4d41a72c488fb0e
Fernando B Oliveira <fboliveira25@gmail.com>
2023-03-13 14:41:47 -0300

    Add CRUD cidades: get all

--- src/routes/cidades.js
+++ src/routes/cidades.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { CreateCidadeController } from '../controller/cidades/CreateCidadeController.js';
+import { GetAllCidadeController } from '../controller/cidades/GetAllCidadeController.js';
 
 const cidadeRouter = Router();
 
@@ -7,7 +8,9 @@ const cidadeRouter = Router();
 const createCidadeController = new CreateCidadeController();
 cidadeRouter.post('/cidades', createCidadeController.handle);
 
-// Get
+// Get All
+const getAllCidadeController = new GetAllCidadeController();
+cidadeRouter.get('/cidades', getAllCidadeController.handle);
 
 // Get By Id
 
